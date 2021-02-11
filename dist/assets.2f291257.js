fa37160b5b58dbe03e0e2f25d31a14c069e62a77
Zizu <angelzizu06@gmail.com>
2021-02-10 20:13:11 -0500

    add more typescript 1.2

--- dist/assets.2f291257.js
+++ dist/assets.2f291257.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42087" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35779" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
